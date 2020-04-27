b6f24a217493192815462a00e1a8c1570f0d072d
arainay <arainay@gmail.com>
2020-04-27 17:04:14 +0800

    Added SSR

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <h1>App</h1>
+  );
+};
+
+export default Home;
--- src/components/PrefetchedComponent/PrefetchedComponent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const PrefetchedComponent = () => <h1>Lazy loaded component with prefetch</h1>;
-
-export default PrefetchedComponent;
